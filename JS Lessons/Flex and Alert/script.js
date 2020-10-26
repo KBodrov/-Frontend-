fc990f6846e76e311328db33b43a16b442671a56
Bodrov Kirill <kbodrov5@gmail.com>
2020-10-26 04:20:49 +0300

    First try of JS

---
+++ JS Lessons/Flex and Alert/script.js
@@ -0,0 +1,14 @@
+
+document.getElementById("1").onclick = function() {myAlert("1")};
+document.getElementById("2").onclick = function() {myAlert("2")};
+document.getElementById("3").onclick = function() {myAlert("3")};
+document.getElementById("4").onclick = function() {myAlert("4")};
+document.getElementById("5").onclick = function() {myAlert("5")};
+document.getElementById("6").onclick = function() {myAlert("6")};
+document.getElementById("7").onclick = function() {myAlert("7")};
+document.getElementById("8").onclick = function() {myAlert("8")};
+document.getElementById("9").onclick = function() {myAlert("9")};
+
+function myAlert(string) {
+    alert(string);
+}
