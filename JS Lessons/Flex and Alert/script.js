88d0f0bb5b0a0989728ce185e17eb1a454cf6e9b
Bodrov Kirill <kbodrov5@gmail.com>
2020-10-28 03:18:27 +0300

    Used FOR in JS-script to reduce code

--- JS Lessons/Flex and Alert/script.js
+++ JS Lessons/Flex and Alert/script.js
@@ -1,13 +1,6 @@
-
-document.getElementById("1").onclick = function() {myAlert("1")};
-document.getElementById("2").onclick = function() {myAlert("2")};
-document.getElementById("3").onclick = function() {myAlert("3")};
-document.getElementById("4").onclick = function() {myAlert("4")};
-document.getElementById("5").onclick = function() {myAlert("5")};
-document.getElementById("6").onclick = function() {myAlert("6")};
-document.getElementById("7").onclick = function() {myAlert("7")};
-document.getElementById("8").onclick = function() {myAlert("8")};
-document.getElementById("9").onclick = function() {myAlert("9")};
+for (let number = 1; number < 10; number++) {
+    document.getElementById(number).onclick = function() {myAlert(number)};
+}
 
 function myAlert(string) {
     alert(string);
