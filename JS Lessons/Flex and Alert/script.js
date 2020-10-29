4a1c2b244b254b5a887292aaaf2ce00ebb923c14
Bodrov Kirill <kbodrov5@gmail.com>
2020-10-29 04:41:58 +0300

    First attempt on adding DIV with JS

--- JS Lessons/Flex and Alert/script.js
+++ JS Lessons/Flex and Alert/script.js
@@ -1,8 +1,34 @@
-let numberOfFields = document.getElementsByClassName("field").length;
-console.log(numberOfFields);
 
-for (let number = 1; number < numberOfFields+1; number++) {
-    document.getElementById(number).onclick = function() {myAlert(number)};
+addField();
+let numberOfFields = countFields();
+console.log(numberOfFields);
+assignAlerts();
+
+function addField() {
+    let row = document.createElement("DIV");
+    row.setAttribute('class','row');
+
+    let field = document.createElement("DIV");
+    field.setAttribute('class','field');
+    field.setAttribute('id', '13');
+
+    let textNode = document.createTextNode("One");
+    
+    field.appendChild(textNode);
+    row.appendChild(field);
+
+    document.getElementById("main").appendChild(row);
+}
+
+function countFields() {
+    return document.getElementsByClassName("field").length;
+}
+
+function assignAlerts() {
+    for (let number = 1; number < numberOfFields+1; number++) {
+        document.getElementById(number).onclick = function() {myAlert(number)};
+    }
+    
 }
 
 function myAlert(string) {
