782cfec13a1a11a7757de8161eeb043e03995857
Bodrov Kirill <kbodrov5@gmail.com>
2021-01-31 21:35:38 +0300

    First success that hard to see =)

--- Librarium/Styles Collection/script.js
+++ Librarium/Styles Collection/script.js
@@ -9,10 +9,26 @@ menuSidebar.addEventListener("click", clickHandler);
 
 function clickHandler(event) {
     if (event.target.hasAttribute("role")) { 
-        console.log(event.target.getAttribute("role"));
+        //console.log(event.target.getAttribute("role"));
+        returnHTML("./" + event.target.getAttribute("role"));
     }
 }
 
-// const menuBtn = documenent.getElementsByClassName("clickable-menu-btn");
+const mainContent = document.getElementById("main-content");
+
+function returnHTML(file_id) {
+    axios.get(file_id)
+    .then(function (response) {
+    
+    mainContent.innerHTML = response.data;
+    
+    console.log(response.data);
+    })
+    .catch(function (error) {
+    console.log(error);
+    });
+}
+
+
 
 
