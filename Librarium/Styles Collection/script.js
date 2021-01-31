d560ec03d2e9f4f346330fa4bc1c659cdcebf046
Bodrov Kirill <kbodrov5@gmail.com>
2021-01-31 20:55:29 +0300

    Preparing HTML and JS files. First test of click handling

--- Librarium/Styles Collection/script.js
+++ Librarium/Styles Collection/script.js
@@ -1,5 +1,18 @@
-const menuBtn = document.getElementById("sidebar_btn");
+const menuArrow = document.getElementById("sidebar_btn");
 
-menuBtn.addEventListener("click", () => {
-    menuBtn.classList.toggle("fa-chevron-right");
+menuArrow.addEventListener("click", () => {
+    menuArrow.classList.toggle("fa-chevron-right");
 });
+
+const menuSidebar = document.getElementById("sidebar");
+menuSidebar.addEventListener("click", clickHandler);
+
+function clickHandler(event) {
+    if (event.target.hasAttribute("role")) { 
+        console.log(event.target.getAttribute("role"));
+    }
+}
+
+// const menuBtn = documenent.getElementsByClassName("clickable-menu-btn");
+
+
