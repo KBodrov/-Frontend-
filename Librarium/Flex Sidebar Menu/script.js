bb6ed640ab8ffb196d08abca3ac76793116fa0d0
Bodrov Kirill <kbodrov5@gmail.com>
2021-02-02 02:21:16 +0300

    Now arros is a btn and you can click on it

--- Librarium/Flex Sidebar Menu/script.js
+++ Librarium/Flex Sidebar Menu/script.js
@@ -1,6 +1,7 @@
+const hideSidebarBtn = document.getElementById("hide_sidebar_btn");
 const menuArrow = document.getElementById("sidebar_btn");
 
-menuArrow.addEventListener("click", () => {
+hideSidebarBtn.addEventListener("click", () => {
     menuArrow.classList.toggle("fa-chevron-right");
 });
 
